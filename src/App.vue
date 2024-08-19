50c12649496cf951093493cf5103104ad6ab11fe
Dome <dome@peerapongs-MacBook-Air.local>
2024-08-19 21:56:42 +0700

    init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import liff from '@line/liff';
+
+const liffInit = async () => {
+  try {
+    await liff.init({ liffId: '2006119518-8daoqjQj' })
+    await liff.ready
+    if (!liff.isLoggedIn()) {
+      liff.login()
+    }
+  } catch (error) {
+    console.error('[ERROR] liff init =>', error)
+  }
+}
+
+onMounted(() => {
+  liffInit()
+})
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <div>
+      <div class="qr-text">
+        {{ scanResult }}
+      </div>
+      <button class="btn" @click="openScanner()">
+        Button
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import liff from '@line/liff';
+
+const scanResult = ref('')
+
+const openScanner = async () => {
+  try {
+    const result = await liff.scanCodeV2()
+    scanResult.value = result.value
+    alert('scan สำเร็จ')
+  } catch (error) {
+    console.error('[ERROR] open scanner =>', error)
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  max-width: 500;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.btn {
+  padding: 16px;
+  height: 40px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.qr-text {
+  padding: 16px;
+  margin-bottom: 16px;
+  border: 1px solid black;
+}
+</style>
